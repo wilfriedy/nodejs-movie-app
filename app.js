5d719a10ff61b74b5c0b5460b8e91b3261c66011
boydeveloper <wilfriedakpe3@gmail.com>
2022-04-03 18:52:02 +0100

    api config and routing

--- app.js
+++ app.js
@@ -11,4 +11,14 @@ app.get("/search", (req, res) => {
   res.render("search");
 });
 
+app.get("/results", (req, res) => {
+  let query = req.query.search;
+  let API_URL = `https://api.themoviedb.org/3/movie/550?api_key=cc7865530fd3f81090af95c598709e32&query=${query}`;
+  request(API_URL, (err, res, body) => {
+    if (err) console.log(err);
+    let data = JSON.parse(body);
+    res.render(data);
+  });
+});
+
 app.listen(3000, console.log("Server is running"));
